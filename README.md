ed5d8794b15b414e48071ca867d9ee5a6f97bdd7
Simon Ittmann <simon.ittmann@byteleaf.de>
2020-06-29 13:41:55 +0200

    Initial maven setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Companyon
+
+## TODO
+
++ Add documentation
++ 
---
+++ src/main/kotlin/de/byteleaf/companyon/CompanyonApplication.kt
@@ -0,0 +1,11 @@
+package de.byteleaf.companyon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompanyonApplication
+
+fun main(args: Array<String>) {
+    runApplication<CompanyonApplication>(*args)
+}
