be043ba770cf3fd12043fea0ace11ab9ecc8fe8a
Jakob Hirscheider <jakob.hirscheider@byteleaf.de>
2020-07-11 14:18:55 +0200

    Add junit test

---
+++ src/test/kotlin/de/byteleaf/companyon/user/control/UserService.kt
@@ -0,0 +1,16 @@
+package de.byteleaf.companyon.user.control
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import de.byteleaf.companyon.user.dto.User
+
+@SpringBootTest
+class DeepRecursiveTest {
+
+    @Test
+    fun getCurrentUser() {
+        // just returns a value without any recursive calls
+        assertEquals(User("99", "Jeff", "Bytezos"), 2)
+    }
+}
