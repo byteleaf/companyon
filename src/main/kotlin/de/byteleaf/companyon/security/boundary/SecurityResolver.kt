af9c3861d0886d32264e2ce21728769857ae362b
Simon Ittmann <simon.ittmann@byteleaf.de>
2021-01-11 22:49:18 +0100

    trigger build

--- src/main/kotlin/de/byteleaf/companyon/security/boundary/SecurityResolver.kt
+++ src/main/kotlin/de/byteleaf/companyon/security/boundary/SecurityResolver.kt
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import graphql.kickstart.tools.GraphQLQueryResolver
 
-
 @Controller
 class SecurityResolver: GraphQLQueryResolver {
 
