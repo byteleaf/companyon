4d75a7f1916d992f21318075403c6d32b3dc08b2
Simon Ittmann <simon.ittmann@byteleaf.de>
2021-01-11 14:24:20 +0100

    comments resolved

--- src/test/kotlin/de/byteleaf/companyon/security/converter/JwtMock.kt
+++ src/test/kotlin/de/byteleaf/companyon/security/converter/JwtMock.kt
@@ -1,6 +1,10 @@
 package de.byteleaf.companyon.security.converter
 import org.springframework.security.oauth2.jwt.Jwt
 
+/**
+ * The only important parameter is the [subjectMock], everything else won't be used is just there to keep the tests running,
+ * empty maps are not working!
+ */
 class JwtMock(val subjectMock: String): Jwt("a", null, null, mapOf(Pair("a", "b")), mapOf(Pair("a", "b"))) {
 
 
