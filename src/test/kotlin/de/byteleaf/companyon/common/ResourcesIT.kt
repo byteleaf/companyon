554c54d82d5d3db5bc44247f4cb1c89989fec9ec
Paul Tolstoi <paul.tolstoi@byteleaf.de>
2021-01-14 12:09:59 +0100

    fix fallback resource not working

---
+++ src/test/kotlin/de/byteleaf/companyon/common/ResourcesIT.kt
@@ -0,0 +1,60 @@
+package de.byteleaf.companyon.common
+
+import de.byteleaf.companyon.AbstractIT
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+
+@AutoConfigureWebClient(registerRestTemplate = true)
+class ResourcesIT : AbstractIT("user") {
+
+    @Autowired
+    protected lateinit var testRestTemplate: TestRestTemplate
+
+    @Test
+    fun testFallbackIndex() {
+        val response = testRestTemplate.exchange("/", HttpMethod.GET, null, String::class.java)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.body).contains("Fallback File")
+    }
+
+    @Test
+    fun testFallbackSubroute() {
+        val response = testRestTemplate.exchange("/dashboard", HttpMethod.GET, null, String::class.java)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.body).contains("Fallback File")
+    }
+
+    @Test
+    fun testExistingFileInSubfolder() {
+        val response = testRestTemplate.exchange("/subfolder/file.txt", HttpMethod.GET, null, String::class.java)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.body).contains("file in a subfolder")
+    }
+
+    @Test
+    fun testGraphQLEndpoint() {
+        val request = HttpEntity("""{ "query": "query test { __typename }" }""")
+
+        val response = testRestTemplate.exchange("/graphql", HttpMethod.POST, request, String::class.java)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.body).contains("""{"data":{"__typename":"Query"}}""")
+    }
+
+    @Test
+    fun testGraphQLPlayground() {
+        val response = testRestTemplate.exchange("/playground", HttpMethod.GET, null, String::class.java)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.body).contains("GraphQL Playground")
+    }
+}
