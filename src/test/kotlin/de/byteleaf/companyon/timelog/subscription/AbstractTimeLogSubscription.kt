55a34a9d97b0e82d398ab297e1a3119a16df51e2
Simon Ittmann <simon.ittmann@byteleaf.de>
2021-03-02 13:10:39 +0100

    conflicts solved

--- src/test/kotlin/de/byteleaf/companyon/timelog/subscription/AbstractTimeLogSubscription.kt
+++ src/test/kotlin/de/byteleaf/companyon/timelog/subscription/AbstractTimeLogSubscription.kt
@@ -25,5 +25,5 @@ class AbstractTimeLogSubscription : AbstractIT("time-log") {
     protected fun createTimeLog(userId: String = NonSecConfiguration.NON_SEC_USER_ID) = performGQLByInput("CreateTimeLog", mapOf(Pair("start", "2011-12-03T10:15:30+01:00"),
         Pair("user", userId),
         Pair("project", "55645"), Pair("description", "A"), Pair("durationInMinutes", 60), Pair("beakInMinutes", 15)))
-        .get("$.data.createTimeLog", targetClass)
+        .get("$.data.createTimeLog", TimeLogGQLResponse::class.java)
 }
