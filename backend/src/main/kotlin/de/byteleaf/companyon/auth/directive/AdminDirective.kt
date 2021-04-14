a0eeb5704abfd62e767a0c272afc55c4db29159c
Simon Ittmann <simon.ittmann@byteleaf.de>
2021-04-14 19:31:44 +0200

    new try

--- backend/src/main/kotlin/de/byteleaf/companyon/auth/directive/AdminDirective.kt
+++ backend/src/main/kotlin/de/byteleaf/companyon/auth/directive/AdminDirective.kt
@@ -3,6 +3,7 @@ package de.byteleaf.companyon.auth.directive
 import graphql.schema.DataFetcherFactories.wrapDataFetcher
 import graphql.schema.DataFetchingEnvironment
 import graphql.schema.GraphQLArgument
+import graphql.schema.GraphQLFieldDefinition
 import graphql.schema.idl.SchemaDirectiveWiring
 import graphql.schema.idl.SchemaDirectiveWiringEnvironment
 import java.util.function.BiFunction
@@ -11,34 +12,24 @@ import java.util.function.BiFunction
 class AdminDirective : SchemaDirectiveWiring {
 
     override fun onArgument(environment: SchemaDirectiveWiringEnvironment<GraphQLArgument>): GraphQLArgument? {
-        val field = environment.element
-        val b = BiFunction { dataFetchingEnvironment: DataFetchingEnvironment, param: Any ->
-            45 as Any
-        }
-
-        //   throw FatalException("asdsd")
-        val dataFetcher = wrapDataFetcher(environment.fieldDataFetcher, b)
-        val currentDF = environment.codeRegistry.getDataFetcher(environment.fieldsContainer, environment.fieldDefinition)
-
-//        return field.transform({ builder -> builder.dataFetcher(dataFetcher) });
-        environment.codeRegistry.dataFetcher(environment.getFieldsContainer(), environment.getFieldDefinition(), ValidationDataFetcher(currentDF));
-        //field.transform { builder: GraphQLArgument.Builder -> builder. }
-        return environment.getElement()
+        val currentDataFetcher = environment.codeRegistry.getDataFetcher(environment.fieldsContainer, environment.fieldDefinition)
+        environment.codeRegistry.dataFetcher(environment.fieldsContainer, environment.fieldDefinition, AuthenticationDataFetcher(currentDataFetcher))
+        return super.onArgument(environment)
     }
 
-//    override fun onArgument(environment: SchemaDirectiveWiringEnvironment<GraphQLArgument>): GraphQLArgument? {
-//        val field = environment.element
-//        val b = BiFunction { dataFetchingEnvironment: DataFetchingEnvironment, param: Any ->
-//            45 as Any
-//        }
-//
-//        //   throw FatalException("asdsd")
-//        val dataFetcher = wrapDataFetcher(environment.fieldDataFetcher, b)
-////        return field.transform({ builder -> builder.dataFetcher(dataFetcher) });
-//        val a = environment.codeRegistry
-//        environment.codeRegistry.dataFetcher(environment.getFieldsContainer(), environment.getFieldDefinition(), dataFetcher);
-//        //field.transform { builder: GraphQLArgument.Builder -> builder. }
-//        return environment.getElement()
-//    }
 
+    override fun onField(environment: SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition>): GraphQLFieldDefinition {
+
+
+        val field = environment.element
+        val b = BiFunction { dataFetchingEnvironment: DataFetchingEnvironment, param: Any ->
+            
+
+            45 as Any
+
+        }
+
+        val dataFetcher = wrapDataFetcher(environment.fieldDataFetcher, b)
+        return field.transform({ builder -> builder.dataFetcher(dataFetcher) });
+    }
 }
--- backend/src/main/kotlin/de/byteleaf/companyon/auth/directive/AuthenticationDataFetcher.kt
+++ backend/src/main/kotlin/de/byteleaf/companyon/auth/directive/AuthenticationDataFetcher.kt
@@ -1,14 +1,14 @@
 package de.byteleaf.companyon.auth.directive
 
-import de.byteleaf.companyon.common.error.exception.FatalException
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
 
-class ValidationDataFetcher(val defaultDataFetcher: DataFetcher<*>) : DataFetcher<Any> {
+class AuthenticationDataFetcher(private val defaultDataFetcher: DataFetcher<*>) : DataFetcher<Any> {
 
 
     override fun get(environment: DataFetchingEnvironment): Any {
-        throw FatalException("asdasd")
+        
+
         return defaultDataFetcher.get(environment)
     }
 
