480d682483995dfe070b5c618a4d52f38cc47e67
Simon Ittmann <simon.ittmann@byteleaf.de>
2021-10-03 14:55:04 +0200

    seeding adjusted

--- backend/src/main/kotlin/de/byteleaf/companyon/SampleData.kt
+++ backend/src/main/kotlin/de/byteleaf/companyon/SampleData.kt
@@ -48,8 +48,7 @@ class SampleData : ApplicationRunner {
         userService.deleteAll()
         userService.create(UserInput("Markus", "Heer", "markus.heer@byteleaf.de", true))
         userService.create(UserInput("Simon", "Ittmann", "simon.ittmann@byteleaf.de", true))
-        userService.create(UserInput("Anthony", "Potdevin", "anthony.potdevin@byteleaf.de", true))
         userService.create(UserInput("Paul", "Tolstoi", "paul.tolstoi@byteleaf.de", true))
-        userService.create(UserInput("Stefan", "Sauterleute", "stefan.sauterleute@byteleaf.de", true))
+        userService.create(UserInput("Ulrich", "Horn", "ulrich.horn@byteleaf.de", true))
     }
 }
